a66a40b1ae339215e5ff28f6ef03a64fc30a433d
moutiasaad <moutiasaad481@gmail.com>
2024-05-02 11:35:55 +0100

    upadate demande

--- src/app/views/demande/demande.component.ts
+++ src/app/views/demande/demande.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+interface SampleData {
+  clientName: string;
+  demande: {
+    DemandeID: number;
+    DemandePour: string;
+    EnvoyeAuLaboratoire: string;
+    BonDeCommande: string;
+    LangueDuCertificat: string;
+  };
+  echantillon: {
+    EchantillonID: number;
+    Gabarit: string;
+    TypeEchantillon: string;
+  };
+  parameter: {
+    ParameterID: number;
+    Name: string;
+  };
+}
 
 @Component({
   selector: 'app-demande',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './demande.component.html',
   styleUrl: './demande.component.scss'
 })
 export class DemandeComponent {
+  combinedData: SampleData[] = [];
 
+  // Define methods
+  accept(data: SampleData): void {
+    console.log('Accepting', data);
+  }
+
+  refuse(data: SampleData): void {
+    console.log('Refusing', data);
+  }
+
+  moreInfo(data: SampleData): void {
+    console.log('More Info for', data);
+  }
 }
