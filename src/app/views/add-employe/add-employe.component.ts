0c947f2a7596da4b2a2a49c062acf0e3c67d2de9
yassinehammami <yassinehammami09@gmail.com>
2024-05-02 11:36:48 +0100

    update

--- src/app/views/add-employe/add-employe.component.ts
+++ src/app/views/add-employe/add-employe.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employe',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-employe.component.html',
-  styleUrl: './add-employe.component.scss'
+  styleUrls: ['./add-employe.component.scss']
 })
 export class AddEmployeComponent {
+  formData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmpassword: '',
+    calcification: '',
+    startDate: '',
+    specialite: ''
+  };
 
+  handleSubmit() {
+    console.log('Form submitted:', this.formData);
+  }
 }
